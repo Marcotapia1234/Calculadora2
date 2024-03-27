9c9cff2b5624664a508fd7f19d6f83f2b522dc83
Marco <empleomarcotapia@gmail.com>
2024-03-27 07:31:52 -0700

    Initial Commit

---
+++ Calculadora/CalculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculadoraApp.swift
+//  Calculadora
+//
+//  Created by Marco on 27/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
